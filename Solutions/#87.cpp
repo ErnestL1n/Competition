87e2aac463ebcd966f3079167776d3dee396e9e0
ErnestL1n <ernestlin1998@gmail.com>
2021-09-20 17:36:54 +0800

    update

--- Solutions/#87.cpp
+++ Solutions/#87.cpp
@@ -7,7 +7,7 @@ int foo(){
     cin >> N >> a >> b;
     cin >> s;
     if(b<0){
-        int cnt=0;
+        int cnt=1;
         for(int i=1;i<s.size();++i)
             if(s[i]!=s[i-1])
                 ++cnt;
