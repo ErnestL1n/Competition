cfcc785bf5fa9b737f9a203907f9c706382aa7bc
ErnestL1n <ernestlin1998@gmail.com>
2021-09-23 14:52:48 +0800

    Create c412.cpp

---
+++ Practice/c412.cpp
@@ -0,0 +1,31 @@
+# include <bits/stdc++.h>
+using namespace std;
+#define mod 1e9+7;
+
+void dfs(int& res,string& s,string& path,int start){
+    if(path=="OwO"){
+        ++res;
+        return;
+    }
+    for(int i=st;i<s.size();++i){
+        if()
+    }
+}
+
+void foo(){
+    string s;
+    cin >> s;
+    int res=0;
+    dfs(res,s,"",0);
+    cout << res;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        foo();
+    return 0;
+}
