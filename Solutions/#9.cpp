20c24329c932c3cb67316cbf8041e023cd3fd369
ErnestL1n <ernestlin1998@gmail.com>
2021-10-03 16:21:13 +0800

    u

--- Solutions/#9.cpp
+++ Solutions/#9.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-const int maxn=1e5+20;
 
 // brute force => TLE
 // int data[maxn<<2];
@@ -11,7 +8,7 @@ const int maxn=1e5+20;
 //     cin.tie(0);
 //     cout.tie(0);
 //     int n,m,type,l,r;
-//     cin >> n >>m;   
+//     cin >> n >>m;
 //     for(int i=1;i<=n;++i)
 //         cin >> data[i];
 
@@ -30,50 +27,45 @@ const int maxn=1e5+20;
 //     return 0;
 // }
 
-int seg[maxn<<2];
-
-
-void update(int index,int val,int l,int r,int n){
-    if(l==r)
-        seg[n]=val;
-    else{
-        int mid=(l+r)>>1,leftChild=n<<1,rightChild=leftChild|1; //or rightChild=leftChild+1
-        if(index<=mid)
-            update(index,val,l,mid,leftChild);
-        else 
-            update(index,val,mid+1,r,rightChild);
-        seg[n]=max(seg[leftChild],seg[rightChild]);
-    }
+int* segTree;
+void update(int idx, int val, int l, int r, int segidx) {
+  if (l == r)
+    segTree[segidx] = val;
+  else {
+    int mid = (l + r) / 2, leftson = segidx * 2, rightson = segidx * 2 + 1;
+    if (idx <= mid)
+      update(idx, val, l, mid, leftson);
+    else
+      update(idx, val, mid + 1, r, rightson);
+    segTree[segidx] = max(segTree[leftson], segTree[rightson]);
+  }
 }
 
-int query(int currL,int currR,int l,int r,int n){
-    if(currL<=l&&r<=currR)
-        return seg[n];
-    int mid=(l+r)>>1,leftChild=n<<1,rightChild=leftChild|1;
-    // since mid=(l+r)/2  => break range and satisfy if statement will be mid<l
-    // since if r=l+1 => (r+l)>>1 =l not r
-    if(mid<currL)
-        return query(currL,currR,mid+1,r,rightChild);
-    if(mid>=currR)
-        return query(currL,currR,l,mid,leftChild);
-    return max(query(currL,currR,mid+1,r,rightChild),query(currL,currR,l,mid,leftChild));
+int query(int left, int right, int l, int r, int segidx) {
+  if (left <= l and right >= r) return segTree[segidx];
+  int mid = (l + r) / 2, leftson = segidx * 2, rightson = segidx * 2 + 1;
+  if (right <= mid) return query(left, right, l, mid, leftson);
+  if (left > mid) return query(left, right, mid + 1, r, rightson);
+  return max(query(left, right, l, mid, leftson),
+             query(left, right, mid + 1, r, rightson));
 }
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n,m,l,r,type,val;
-    cin >> n >>m;
-    for(int i=1;i<=n;++i){
-        cin >>val;
-        update(i,val,1,n,1);
-    }
-    while(m--){
-        cin >> type >> l >>r;
-        if(type==1)
-            update(l,r,1,n,1);
-        else 
-            cout << query(l,r,1,n,1) <<"\n";
-    }
-    return 0;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n, m, l, r, type, val;
+  cin >> n >> m;
+  segTree = (int*)calloc(n * 4, sizeof(int));
+  for (int i = 1; i <= n; ++i) {
+    cin >> val;
+    update(i, val, 1, n, 1);
+  }
+  while (m--) {
+    cin >> type >> l >> r;
+    if (type == 1)
+      update(l, r, 1, n, 1);
+    else
+      cout << query(l, r, 1, n, 1) << "\n";
+  }
+  return 0;
 }
